090049b6025c3449addefbfb30165412fadfe740
Nicolas <31369184+nre0104@users.noreply.github.com>
2020-05-26 12:46:10 +0200

    Implemented Time Skill

---
+++ Assets/Scripts/PlayerSkillsManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerSkillsManager : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void PlaceIce()
+        {
+
+        }
+
+        void SlowTime()
+        {
+            if (Time.timeScale == 1f)
+            {
+                Time.timeScale = 0.25f;
+
+            }
+        }
+
+        void SpeedUpTime()
+        {
+
+            if (Time.timeScale == 1f)
+            {
+                Time.timeScale = 1.5f;
+
+            }
+        }
+
+        void SetStandardTime()
+        {
+            if (Time.timeScale != 1.0f)
+            {
+                Time.timeScale = 1f;
+            }
+        }
+
+        void ThrowBomb()
+        {
+
+        }
+    }
+}
