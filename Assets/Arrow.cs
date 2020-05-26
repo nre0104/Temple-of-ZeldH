3a293e81cc6f1ef1c1cbd4cc34669151bdc076c5
Isi <Isidoros@gmx.de>
2020-05-26 13:52:35 +0200

    Added shooting Mechanik and Zeld Sword Asset

---
+++ Assets/Arrow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    Rigidbody myBody;
+    private float lifeTimer = 3f;
+    private float timer;
+    private bool hitSomething = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.LookRotation(myBody.velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer>= lifeTimer)
+        {
+            Destroy(gameObject);
+        }
+
+        if (!hitSomething)
+        {
+            transform.rotation = Quaternion.LookRotation(myBody.velocity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag != "Arrow")
+        {
+            hitSomething = true;
+            Stick();
+        }
+    }
+
+    private void Stick()
+    {
+        myBody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public float PlayerHealt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
