34fa6e2655c5bdc86b28244d7370547efdf2fb31
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-05-28 01:02:08 +0200

    added Sound to Buttons

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+    public string sceneStr;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You have quit the game");
+        if (UnityEditor.EditorApplication.isPlaying == true)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("LoadScene");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneStr);
+    }
+}
