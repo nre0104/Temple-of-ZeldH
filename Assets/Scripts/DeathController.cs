9bca4d78123fee7a01f6ef52cb1c5edf6947f8af
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-05-28 22:43:13 +0200

    added DeathCollider

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.gameObject.name.Contains("Player 2"))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
