084d100bff5d80b04d4a35de1ab9a4c9e5922c0d
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-05-28 18:38:27 +0200

    Level 1 with Lecel 2 connected

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+   
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.gameObject.name.Contains("Player") && 
+          SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if(col.transform.gameObject.name.Contains("Player") &&
+          SceneManager.GetActiveScene().buildIndex == 3)
+        {
+            MenuPause.Instance.sceneStr = "StartGameMenu";
+            MenuPause.Instance.LoadMenu();
+        }
+
+    }
+}
