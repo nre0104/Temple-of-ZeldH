c9cd4386f8e0fbc1bde0c9fc5c5e41e28232383d
Nicolas Resch <nresch@stud.hs-heilbronn.de>
2020-05-28 11:50:19 +0200

    Fixed puddle bug by new skript

---
+++ Assets/Scripts/PuddleController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PuddleController : MonoBehaviour
+    {
+
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Trigger Enter");
+            gameObject.GetComponent<Collider>().isTrigger = false;
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            Debug.Log("Trigger Exit");
+            gameObject.GetComponent<Collider>().isTrigger = true;
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
